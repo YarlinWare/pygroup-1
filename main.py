0e425cb40c8345057a0133cbb2bde455172a4ce5
Oscar Dowson <o.dowson@gmail.com>
2014-11-05 08:24:21 +1300

    solves. working on creating quality report

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def createGroups(n_groups, n_entities):
     k = n_groups - int(n_entities - int(n_entities / n_groups) * n_groups)
     group_size = dict()
     for g in GROUPS:
-        if g<k:
+        if g<=k:
             group_size[g] = int(n_entities/n_groups)
         else:
             group_size[g] = int(n_entities/n_groups) + 1
@@ -48,12 +48,12 @@ def addEntityConstraints(model, variables, ENTITIES, GROUPS):
 
 def addNumericConstraints(model, data, variables, numerical, GROUPS, group_size):
     for g in GROUPS:
-        model += lpSum([variables['x'][(d['ID'], g)] for d in data]) == group_size[g]
+        model += lpSum([variables['x'][(d['ID'], g)] for d in data]) == group_size[g], '%s' % g
         for v in numerical:
-            model += lpSum([d[v]*variables['x'][(d['ID'], g)] for d in data]) >= group_size[g] * variables[v]['mean_min']
-            model += lpSum([d[v]*variables['x'][(d['ID'], g)] for d in data]) <= group_size[g] * variables[v]['mean_max']
-            model += lpSum([pow(d[v] - numerical[v]['mean'], 2)*variables['x'][(d['ID'], g)] for d in data]) >= group_size[g] * variables[v]['var_min']
-            model += lpSum([pow(d[v] - numerical[v]['mean'], 2)*variables['x'][(d['ID'], g)] for d in data]) <= group_size[g] * variables[v]['var_max']
+            model += lpSum([d[v] * variables['x'][(d['ID'], g)] for d in data]) >= group_size[g] * variables[v]['mean_min']
+            model += lpSum([d[v] * variables['x'][(d['ID'], g)] for d in data]) <= group_size[g] * variables[v]['mean_max']
+            model += lpSum([pow(d[v] - numerical[v]['mean'], 2) * variables['x'][(d['ID'], g)] for d in data]) >= group_size[g] * variables[v]['var_min']
+            model += lpSum([pow(d[v] - numerical[v]['mean'], 2) * variables['x'][(d['ID'], g)] for d in data]) <= group_size[g] * variables[v]['var_max']
     return model
 
 
@@ -73,7 +73,7 @@ def createProblem(data, categorical, numerical, n_groups):
     tuples = dict()
     tuples['entity'] = [(e, g) for e in ENTITIES for g in GROUPS]
     for c in categorical:
-        tuples[c] = [(i, g) for i in categorical[c] for g in GROUPS]
+        tuples[c] = [(i, g) for (i, n) in categorical[c] for g in GROUPS]
 
     model = LpProblem('Partition', LpMinimize)
     variables = createVariables(categorical, numerical, tuples)
@@ -81,24 +81,48 @@ def createProblem(data, categorical, numerical, n_groups):
     model = addEntityConstraints(model, variables, ENTITIES, GROUPS)
     model = addNumericConstraints(model, data, variables, numerical, GROUPS, group_size)
     model = addCategoricalConstraints(model, data, variables, categorical, GROUPS, n_entities)
+    return model, variables
 
-    return model
 
+def extractResults(variables):
+    allocation = dict()
+    for (e, g) in variables['x']:
+        if variables['x'][(e, g)].value() == 1:
+            allocation[e] = g
+    return allocation
+
+
+##def getSolutionQuality(variables, categorical, numerical):
+##    quality = {'categorical':dict(), 'numerical':dict()}
+##    for c in categorical:
+##        max_violation = 0
+##        quality['categorical'][c] = dict()
+##        for (l, n) in categorical[c]:
+##            quality['categorical'][c][l]
+
+def getDataFromDB(server, database, entity_data_table, variable_classification_table):
+    con, cursor = db.connectToDatabase(server, database)
+    classification = db.grabCategories(cursor, variable_classification_table)
+    entity_data = db.buildDataDictionary(cursor, entity_data_table)
+    categorical = db.getCategoryLevels(cursor, entity_data_table, classification)
+    numerical = db.getNumericalMetrics(cursor, entity_data_table, classification)
+    db_data = dict()
+    db_data['entity_data'] = entity_data
+    db_data['categorical_data'] = categorical
+    db_data['numerical_data'] = numerical
+    return db_data
+    
 # =================================================
 server = '.'
 database = 'enggen403'
-con, cursor = db.connectToDatabase(server, database)
+entity_data_table = 'class_data'
+variable_classification_table = 'categories'
 
-# =================================================
-classification = db.grabCategories(cursor, 'categories')
-entity_data = db.buildDataDictionary(cursor, 'class_data')
-
-categorical = db.getCategoryLevels(cursor, 'class_data', classification)
-numerical = db.getNumericalMetrics(cursor, 'class_data', classification)
-
-# =================================================
 n_groups = 20
+time_limit = 10
+# =================================================
+db_data = getDataFromDB(server, database, entity_data_table, variable_classification_table)
+model, variables = createProblem(db_data['entity_data'], db_data['categorical_data'], db_data['numerical_data'], n_groups)
+status = model.solve(solvers.PULP_CBC_CMD(maxSeconds=timelimit))
 
-model = createProblem(entity_data, categorical, numerical, n_groups)
-
-model.solve(solvers.PULP_CBC_CMD(keepFiles=1, msg=2, maxSeconds=10))
+allocation = extractResults(variables)
